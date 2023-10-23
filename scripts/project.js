281c64f09b8a9162f520190947d6f8fdea8fa8bb
Bhotton25 <bhotton25@gmail.com>
2023-10-22 20:17:02 -0600

    Had to scrap the scripture idea, created a Pokemon API and all functions are now active

--- scripts/project.js
+++ scripts/project.js
@@ -1,35 +1,55 @@
 //Global variables
-let scriptureList = [];
+let pokemonList;
 let results = document.querySelector("#results");
 
-
-
-
-const getRandomScripture = async () => {
-    const response = await fetch(`https://bible-api.com/?random=verse`, {mode: `no-cors`});
-    if(response.ok){
-        scriptureList = await response.json();
-        return scriptureList;
+const getPokemon = async (name) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
     }
-}
+  );
+  if (response.ok) {
+    pokemonList = await response.json();
+    return pokemonList;
+  }
+};
 
-const displayScriptures = async () => {
-    try {
-        const scriptures = await getRandomScripture();
-        if (scriptures && scriptures.length) {
-            scriptures.forEach(scripture => {
-                let h3 = document.createElement(`h3`);
-                let p = document.createElement(`p`);
-                h3.textContent = scripture.reference;
-                p.textContent = scripture.text;
-                results.appendChild(h3);
-                results.appendChild(p);
-            });
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
-    }
-}
+const displayPokemon = async () => {
+  results.innerHTML = "<h1>Pokemon:</h1>";
+  const name = document.querySelector("#Pokemon").value;
+  let pokemon = await getPokemon(name);
 
-//document.querySelector(`#getScripture`).addEventListener(`click`, getScripture);
-document.querySelector(`#getRandomScripture`).addEventListener(`click`, displayScriptures);
+  //Abilities Builder
+  const abilityNames = pokemon.abilities.map((ability) => ability.ability.name);
+
+  //Stat Builder
+  const statsString = pokemon.stats
+    .map((stat) => `<strong>${stat.stat.name}:</strong> ${stat.base_stat}`)
+    .join("<br>");
+
+  // Extracting height, weight, and types
+  const height = pokemon.height / 10; // Dividing by 10 to convert to meters.
+  const weight = pokemon.weight / 10; // Dividing by 10 to convert to kilograms.
+  const types = pokemon.types.map((type) => type.type.name).join("<br>");
+
+  //Posting to HTML
+  results.innerHTML = `
+        <h3>${pokemon.name.toUpperCase()}</h3>
+        <picture>
+        <img src="${pokemon.sprites.other.home.front_default}" alt="${
+    pokemon.name
+  }">
+        </picture>
+        <p><h4>Height:</h4> ${height} meters</p>
+        <p><h4>Weight:</h4> ${weight} kg</p>
+        <p><h4>Types:</h4> ${types}</p><br>
+        <p><h3>Abilities:<hr></h3> ${abilityNames.join("<br>")}</p>
+        <br>
+        <h3>Stats:<hr></h3>
+        <p>${statsString}</p>
+    `;
+};
+
+document.querySelector(`#getPokemon`).addEventListener(`click`, displayPokemon);
